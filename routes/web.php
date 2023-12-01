a50ef0b94adaebf7e163793e8af10b04fd577814
LilisRisnawatiNurulliyah <LilisRisnawatiNurulliyah>
2023-12-01 11:47:48 +0700

    View Dan Blade Engine Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProdukController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    $data = [
+        'pageTitle' => 'Tentang Kami',
+        'content' => 'Ini adalah halaman tentang kami.'
+    ];
+    return view('about', $data);
+});
+
+
+Route::resource('produk', ProdukController::class);
+Route::resource('users', UserController::class);
+
+Route::get('profile', function () {
+    $nama = "Meike Nadya Chandra";
+    return view('profile', compact('nama'));
+});
+
+Route:: Resource(' /profile', App\Http\Controller\ProfileControllers::class);
